0d8f8e295280c26ee91037375d52c1d5b8cb778e
SURAJ_KUMAR <surajk38656@gmail.com>
2024-08-26 13:34:35 +0530

    FileHandling

--- src/FileHandling/CreateFile.java
+++ src/FileHandling/CreateFile.java
@@ -1,13 +1,20 @@
+package FileHandling;
+
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class CreateFile {
     public static void main(String[] args) {
         try {
-            File myObj = new File("filename.txt");
+            File myObj = new File("./src/FileHandling/filename.txt");
+            File myObj1 = new File("./src/FileHandling/filename1.txt");
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
-            } else {
+            }
+            else if(myObj1.createNewFile()){
+                System.out.println("File created: "+myObj1.getName());
+            }
+            else {
                 System.out.println("File already exists.");
             }
         } catch (IOException e) {
