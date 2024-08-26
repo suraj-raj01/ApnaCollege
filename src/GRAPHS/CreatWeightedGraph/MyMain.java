e8069b0dd65426686e9eb49111c124abaad7eef3
SURAJ_KUMAR <surajk38656@gmail.com>
2024-08-27 01:40:12 +0530

    Weighted Graph

--- src/GRAPHS/CreatWeightedGraph/MyMain.java
+++ src/GRAPHS/CreatWeightedGraph/MyMain.java
@@ -1,7 +1,23 @@
 package GRAPHS.CreatWeightedGraph;
 
+import java.util.ArrayList;
+
 public class MyMain {
     public static void main(String[] args) {
-
+        int V = 4;
+        ArrayList<WeightedGraph.Edge>[] graph = new ArrayList[V];
+        WeightedGraph.weightedGraph(graph);
+        System.out.println("=======================");
+        System.out.println("Source\tDest\tWeight");
+        System.out.println("=======================");
+        for(int i=0; i<graph[2].size(); i++){
+            WeightedGraph.Edge e = graph[2].get(i);
+            System.out.println(e.src+"\t\t"+e.dest+"\t\t"+e.weight);
+        }
+        for(int i=0; i<graph[1].size(); i++){
+            WeightedGraph.Edge e = graph[1].get(i);
+            System.out.println(e.src+"\t\t"+e.dest+"\t\t"+e.weight);
+        }
+        System.out.println("=======================");
     }
 }
--- src/GRAPHS/CreatWeightedGraph/WeightedGraph.java
+++ src/GRAPHS/CreatWeightedGraph/WeightedGraph.java
@@ -1,7 +1,27 @@
 package GRAPHS.CreatWeightedGraph;
 
-public class WeightedGraph {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class WeightedGraph {
+    public static class Edge {
+        int src;
+        int dest;
+        int weight;
+        public Edge(int src, int dest, int weight){
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+    public static void weightedGraph(ArrayList<Edge>[]graph){
+        for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[2].add(new Edge(2,0,2));
+        graph[2].add(new Edge(2,3,-1));
+        graph[2].add(new Edge(2,1,10));
+
+        graph[1].add(new Edge(1,3,0));
+        graph[1].add(new Edge(1,2,10));
     }
 }
